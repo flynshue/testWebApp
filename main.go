050737833303cbeaab263396fefad67a6c9717aa
Felicia Lyn-Shue <flynshue@gmail.com>
2019-01-29 19:18:26 -0500

    Added new views

--- main.go
+++ main.go
@@ -1,37 +1,32 @@
 package main
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 	"os"
-
+	"github.com/flynshuePersonal/testWebApp/views"
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var homeView *views.View
 
 type data struct {
-	Name     string
+	User     string
 	Hostname string
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	hostname := os.Getenv("HOSTNAME")
-	myData := data{Name: "Felicia", Hostname: hostname}
-	if err := homeTemplate.Execute(w, myData); err != nil {
+	username := os.Getenv("USER")
+	myData := data{User: username, Hostname: hostname}
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, myData)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles("views/home.gohtml",
-		"views/layouts/footer.gohtml")
-	if err != nil {
-		log.Fatal("Template ParseFiles Error: ", err)
-	}
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	http.ListenAndServe(":3000", r)
